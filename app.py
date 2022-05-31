2c12841544d1ef4197b221186fb40609f6a6a7ae
Daryl <tsdaryl@gmail.com>
2022-05-30 20:21:29 -0700

    create alert endpoint

--- app.py
+++ app.py
@@ -1,4 +1,10 @@
 from flask import Flask, render_template
+import os
+from dotenv import load_dotenv
+from twilio.rest import Client
+import json
+
+load_dotenv()
 
 app = Flask(__name__, static_url_path="/static")
 
@@ -16,3 +22,20 @@ def demo():
 @app.route("/run")
 def run_application():
     return render_template("run.html")
+
+
+@app.route("/api/alert")
+def alert():
+    account_sid = os.environ["TWILIO_ACCOUNT_SID"]
+    auth_token = os.environ["TWILIO_AUTH_TOKEN"]
+    from_number = os.environ["TWILIO_PHONE_NUMBER"]
+    to_number = os.environ["TARGET_PHONE_NUMBER"]
+
+    client = Client(account_sid, auth_token)
+    client.messages.create(
+        body="Alert triggered by Web Sentinel",
+        from_=from_number,
+        to=to_number,
+    )
+
+    return json.dumps({"alert": True})
